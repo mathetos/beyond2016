ff867ce3c835c64cd157bc4139ea0db199b7f636
mathetos <reachme@mattcromwell.com>
2015-11-21 20:41:18 -0800

    * Added header body class functions back in. * New screenshot.png

--- inc/beyond2016-functions.php
+++ inc/beyond2016-functions.php
@@ -7,3 +7,37 @@ function beyond2016_customizer_enqueue() {
 	wp_enqueue_script( 'beyond2016-customizer', get_template_directory_uri() . '/assets/js/source/beyond2016-customizer.js', array( 'jquery', 'customize-controls' ), false, true );
 }
 add_action( 'customize_controls_enqueue_scripts', 'beyond2016_customizer_enqueue' );
+
+add_filter( 'body_class', 'matt2016_header_body_classess' );
+function matt2016_header_body_classess($classes) {
+   $headeralignment = get_theme_mod( 'header_alignment' );
+   $headerlayout = get_theme_mod( 'header_layout' );
+   $headerimgpos = get_theme_mod( 'header_image_position' );
+   switch ($headeralignment) {
+     case '0' :
+      $classes[] = 'header-left';
+      break;
+     case '1' :
+      $classes[] = 'header-center';
+      break;
+     case '2' :
+      $classes[] = 'header-right';
+      break;
+     default :
+      $classes[] = 'header-left';
+   }
+   switch ($headerimgpos) {
+     case '0' :
+      $classes[] = 'header-image-above';
+      break;
+     case '1' :
+      $classes[] = 'header-image-below';
+      break;
+     case '2' :
+      $classes[] = 'header-image-behind';
+      break;
+     default :
+      $classes[] = 'header-image-below';
+   }
+   return $classes;
+ }
