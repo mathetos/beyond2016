415220e6abd55186e4e582830563cdcb70216f00
Matt Cromwell <reachme@mattcromwell.com>
2016-06-18 21:39:31 -0700

    Cleaned up functions that should be in child-theme instead
    Two functions, exclude password protected posts from archives, and
    display popular post label based on comment count were removed to be
    added into Matt2016.

--- inc/beyond2016-functions.php
+++ inc/beyond2016-functions.php
@@ -6,122 +6,42 @@ require_once BEYOND2016_PATH . '/lib//tgm/class-tgm-plugin-activation.php';
 add_action( 'tgmpa_register', 'beyond2016_register_required_plugins' );
 
 function beyond2016_register_required_plugins() {
-	/*
-	 * Array of plugin arrays. Required keys are name and slug.
-	 * If the source is NOT from the .org repo, then source is also required.
-	 */
+
 	$plugins = array(
 
-		// This is an example of how to include a plugin bundled with a theme.
 		array(
-			'name'               => 'Beyond 2016 Updater Plugin', // The plugin name.
-			'slug'               => 'github-updater', // The plugin slug (typically the folder name).
-			'source'             => 'https://github.com/afragen/github-updater/archive/5.4.1.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '5.4.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			//'external_url'       => 'https://github.com/afragen/github-updater/archive/5.4.1.zip', // If set, overrides default API URL and points to an external URL.
-			//'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			'name'               => 'Beyond 2016 Updater Plugin', 
+			'slug'               => 'github-updater', 
+			'source'             => 'https://github.com/afragen/github-updater/archive/5.4.1.zip', 
+			'required'           => true, 
+			'version'            => '5.4.1', 
+			'force_activation'   => false, 
+			'force_deactivation' => false, 
+		),
+
+		array(
+			'name'      => 'Yoast SEO',
+			'slug'      => 'wordpress-seo',
+			'required'  => false,
 		),
 
 	);
 
-	/*
-	 * Array of configuration settings. Amend each line as needed.
-	 *
-	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
-	 * sending in a pull-request with .po file(s) with the translations.
-	 *
-	 * Only uncomment the strings in the config array if you want to customize the strings.
-	 */
 	$config = array(
-		'id'           => 'beyond2016',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'themes.php',            // Parent menu slug.
-		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => true,                   // Automatically activate plugins after installation or not.
-		'message'      => '<div class="subttitle"><h3><em>The Github Updater plugin allows you to get notified when there are updates available for Beyond 2016.</em></h3></div>',                      // Message to output right before the plugins table.
-
-		
+		'id'           => 'beyond2016',                 
+		'default_path' => '',                      
+		'menu'         => 'tgmpa-install-plugins', 
+		'parent_slug'  => 'themes.php',            
+		'capability'   => 'edit_theme_options',    
+		'has_notices'  => true,                    
+		'dismissable'  => true,                    
+		'dismiss_msg'  => '',                      
+		'is_automatic' => true,                   
+		'message'      => '<div class="subttitle"><p><strong>Github Updater</strong> -- <em>The Github Updater plugin allows you to get notified when there are updates available for Beyond 2016.</em></p><hr /><p><strong>Yoast SEO</strong> -- <em>This is a <u>Suggested</u> plugin requirement because Beyond 2016 has intentional integrations with Yoast SEO. Like using meta-descriptions for archive excerpts and breadcrumbs</em>.</p></div>',                      
 		'strings'      => array(
-			'page_title'                      => __( 'Install the Github Updater to Get Automatic Updates of Beyond 2016', 'beyond2016' ),
-			'menu_title'                      => __( 'Get Theme Updates', 'beyond2016' ),
-			/* translators: %s: plugin name. * /
-			'installing'                      => __( 'Installing Plugin: %s', 'beyond2016' ),
-			/* translators: %s: plugin name. * /
-			'updating'                        => __( 'Updating Plugin: %s', 'beyond2016' ),
-			'oops'                            => __( 'Something went wrong with the plugin API.', 'beyond2016' ),
-			'notice_can_install_required'     => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'This theme requires the following plugin: %1$s.',
-				'This theme requires the following plugins: %1$s.',
-				'beyond2016'
-			),
-			'notice_can_install_recommended'  => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'This theme recommends the following plugin: %1$s.',
-				'This theme recommends the following plugins: %1$s.',
-				'beyond2016'
-			),
-			'notice_ask_to_update'            => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-				'beyond2016'
-			),
-			'notice_ask_to_update_maybe'      => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'There is an update available for: %1$s.',
-				'There are updates available for the following plugins: %1$s.',
-				'beyond2016'
-			),
-			'notice_can_activate_required'    => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following required plugin is currently inactive: %1$s.',
-				'The following required plugins are currently inactive: %1$s.',
-				'beyond2016'
-			),
-			'notice_can_activate_recommended' => _n_noop(
-				/* translators: 1: plugin name(s). * /
-				'The following recommended plugin is currently inactive: %1$s.',
-				'The following recommended plugins are currently inactive: %1$s.',
-				'beyond2016'
-			),
-			'install_link'                    => _n_noop(
-				'Begin installing plugin',
-				'Begin installing plugins',
-				'beyond2016'
-			),
-			'update_link' 					  => _n_noop(
-				'Begin updating plugin',
-				'Begin updating plugins',
-				'beyond2016'
-			),
-			'activate_link'                   => _n_noop(
-				'Begin activating plugin',
-				'Begin activating plugins',
-				'beyond2016'
-			),
-			'return'                          => __( 'Return to Required Plugins Installer', 'beyond2016' ),
-			'plugin_activated'                => __( 'Plugin activated successfully.', 'beyond2016' ),
-			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'beyond2016' ),
-			/* translators: 1: plugin name. * /
-			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'beyond2016' ),
-			/* translators: 1: plugin name. * /
-			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'beyond2016' ),
-			/* translators: 1: dashboard link. * /
-			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'beyond2016' ),
-			'dismiss'                         => __( 'Dismiss this notice', 'beyond2016' ),
-			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'beyond2016' ),
-			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'beyond2016' ),
-
-			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.*/
+			'page_title'   => __( 'Install the Github Updater to Get Automatic Updates of Beyond 2016', 'beyond2016' ),
+			'menu_title'   => __( 'Get Theme Updates', 'beyond2016' ),
+			
 		),
 		
 	);
@@ -179,7 +99,7 @@ function beyond2016_body_classess($classes) {
    }
 	 global $post;
 
-    if (is_404() == false) {
+    if ( is_404() == false ) {
         $sidebar = get_post_meta($post->ID, 'beyond2016-sidebar-layout', true);
     }
 
@@ -244,36 +164,3 @@ function beyond2016_print_archive_grid_init() {
 }
 
 add_action( 'wp_footer', 'beyond2016_print_archive_grid_init' );
-
-/*
- *  Excludes Password Protected posts from the
- *  Archive and Category loops.
- *
- */
-
-function exclude_passworded_posts($post) {
-	global $post;
-	$b16ecom_exclude = post_password_required($post->ID);
-
-	if ($b16ecom_exclude == true) {
-		$excludepass = false;
-	} else {
-		$excludepass = true;
-	}
-	return apply_filters('exclude-passworded-posts', $excludepass);
-}
-
-function b16ecom_comment_count() {
-
-	global $post;
-	$commentsnum = wp_count_comments($post->ID);
-
-	if ($commentsnum->total_comments > 14) {
-		$popular = '<p style=quot;font-size: 80%; font-style: italics; quot;><span class=&quot;genericon genericon-digg&quot;></span>Popular Post :</p>';
-	} else {
-		$popular = '';
-	}
-
-	return $popular;
-
-}
